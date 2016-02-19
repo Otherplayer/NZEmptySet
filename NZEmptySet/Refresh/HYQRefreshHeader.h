8ccad75335112e15e252ef025fe9b234a210c59a
__无邪_ <Fqah@fqahs-MacBook-Pro.local>
2016-02-19 18:46:13 +0800

    first commit

---
+++ NZEmptySet/Refresh/HYQRefreshHeader.h
@@ -0,0 +1,17 @@
+//
+//   /\_/\
+//   \_ _/
+//    / \ not
+//    \_/
+//
+//  Created by __无邪_ on 2/19/16.
+//  Copyright © 2016 fqah. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MJRefresh.h>
+@interface HYQRefreshHeader : MJRefreshGifHeader
+
+// custom code
+
+@end
